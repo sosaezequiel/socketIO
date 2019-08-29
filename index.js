095f2e66a5adaf1a053bcbf70d64bd8389749d55
rsosa <rsosa@mae.com.ar>
2019-08-29 18:21:15 -0300

    casi

--- index.js
+++ index.js
@@ -7,18 +7,28 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-
+var numero_de_clientes = 0;
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
 
 
 io.on('connection', function (socket) {
-  socket.emit('news', { msg: 'desde el server' });
+
+  numero_de_clientes += 1;
+
+  socket.broadcast.emit('conexiones', { type : 1 ,msg: 'nueva conexion ', numero_de_clientes });
+
   socket.on('push', function (data) {
     console.log(data.msg);
   });
+
+  socket.on('disconnect', function () {
+    numero_de_clientes -= 1;
+    socket.broadcast.emit('conexiones', { type : 2 , msg: 'un cliente desconectado ', numero_de_clientes });
+  });
 });
+
--- src/main.js
+++ src/main.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import socketService from './services/socketService';
+import PanelCards from './componets/PanelCards';
 
 import "./css/estilo.scss";
+var namespace = "Demo";
 
 
 
 $.extend(true, window, {
-    MAE: {
+    [namespace]: {
         SocketService: socketService
     }
 });
 
 
-
-function App(props) {
-
-    return "········";
-}
-
-ReactDOM.render(<App />, document.querySelector("#app"));
+ReactDOM.render(<PanelCards />, document.querySelector("#app"));
 
 
 socketService.connect(function () {
 
     console.log("conectado");
-    socketService.subscribe("news", function (data) {
-        console.log(data);
+    socketService.subscribe("conexiones", function (data) {
+        console.dir(data);
         socketService.emit("push", { msg: "desde el cliente atravez de servicio" });
     });
 

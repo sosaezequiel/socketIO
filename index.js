fe5fe2c19d7b557792f5ddd6be193e6b31c486d2
rsosa <rsosa@mae.com.ar>
2019-08-29 13:22:53 -0300

    socket

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
-
-
